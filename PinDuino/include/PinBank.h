e0258e8dd13288c8b9663494125ee7f32eb7fabc
Peter-Frank Spierenburg <spierepf@hotmail.com>
2015-02-24 14:43:42 -0400

    Fixed bug in PinBank

--- PinDuino/include/PinBank.h
+++ PinDuino/include/PinBank.h
@@ -21,11 +21,11 @@ public:
 
 	PinBank(uint8_t port, uint8_t mask) : pinr(&PINB + 3*port), mask(mask) { }
 
-	void dirIn() { pinr[1] &= !mask; }
+	void dirIn() { pinr[1] &= ~mask; }
 	void dirOut() { pinr[1] |= mask; }
 	uint8_t read() { return pinr[0] & mask; }
 	void toggle() { pinr[0] = mask; }
-	void dataLow() { pinr[2] &= !mask; }
+	void dataLow() { pinr[2] &= ~mask; }
 	void dataHigh() { pinr[2] |= mask; }
 };
 
