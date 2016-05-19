e25d15312080041e4699ea3051dcbf970c797695
Peter-Frank Spierenburg <spierepf@hotmail.com>
2016-05-18 21:31:10 -0300

    Fixed whitespace.

--- kingpyn/main.py
+++ kingpyn/main.py
@@ -16,4 +16,4 @@ while True:
     endPointManager.schedule()
     while len(endPointManager.switchEventQueue) > 0:
         log.info("Switch event: {}".format(endPointManager.switchEventQueue.pop()))
-        
+
